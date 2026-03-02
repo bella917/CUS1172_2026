eead89d75b3f1ea8a0cf189d9bd3ab8daa4490f9
bella917 <isabella.militana24@my.stjohns.edu>
2026-03-02 12:21:43 -0500

    Update myjavascript.js

--- Project02/myjavascript.js
+++ Project02/myjavascript.js
@@ -1,1 +1,35 @@
+document.addEventListener("DOMContentLoaded", function () {
 
+let task-array = [];
+
+
+document.querySelector("#task-form").onsubmit = function () {
+
+  let task_title = document.querySelector("#task-title").value;
+
+  let task = {
+    "task-id": task-array.length + 1,
+    "task-name": task_title,
+    "task-priority": "low",
+    "status": "pending"
+};
+
+
+  task-list.push(task);
+  console.log(task-list);
+
+  let li = document.createElement("li");
+  li.innerHTML = task["task-name"];
+
+  document.querySelector("#tasklist").appendChild(li);
+
+  li.onclick = function (event) {
+    console.log(event.target);
+ 
+    return false;
+};
+
+  
+};
+  
+});
