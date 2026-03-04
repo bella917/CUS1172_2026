1743a9ecafcb3a0049af274654e29164c3b5ac21
bella917 <isabella.militana24@my.stjohns.edu>
2026-03-03 20:25:30 -0500

    Update myjavascript.js

--- Project02/myjavascript.js
+++ Project02/myjavascript.js
@@ -21,13 +21,45 @@ document.querySelector("#task-form").onsubmit = function () {
   let li = document.createElement("li");
   li.innerHTML = task["task-name"];
 
-  document.querySelector("#tasklist").appendChild(li);
+
+  let completebutt = document.createElement("button");
+  completebutt.innerHTML = "Complete";
+  completebutt.type = "button";
+  completebutt.style.marginLeft = "10px";
+
+  completebutt.onclick = function () {
+    task["status"] = "completed";
+    li.style.color = "green";
+    console.log(taskarray);
+        };
+  
+  li.appendChild(completebutt);
+
+  let removebutt = document.createElement("button");
+        removebutt.innerHTML = "Remove";
+        removebutt.type = "button";
+        removebutt.style.marginLeft = "5px";
+
+        removebutt.onclick = function () {
+            for (let i = 0; i < taskarray.length; i++) {
+                if (taskarray[i]["task-id"] === task["task-id"]) {
+                    taskarray.splice(i, 1);
+                    break;
+                }
+            }
+
+            li.remove();
+            console.log(taskarray); };
+
+        li.appendChild(removebutt);
+
 
   li.onclick = function (event) {
     console.log(event.target);
  
-    
+     
 };
+document.querySelector("#tasklist").appendChild(li);
 return false;
   
 };
